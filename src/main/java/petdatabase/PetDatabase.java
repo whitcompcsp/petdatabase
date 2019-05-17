bda241f93404e297344b25a8fe06dd8cb20210bf
Paul Whitcomb <whitcomp@csp.edu>
2019-05-17 18:32:23 -0500

    Move the main class into a package

---
+++ src/main/java/petdatabase/PetDatabase.java
@@ -0,0 +1,16 @@
+/*
+ * Paul Whitcomb <whitcomp@csp.edu>
+ * CSC 422, Spring 2019
+ * Concordia University, St. Paul
+ * Week 1 Assignment
+ */
+
+package petdatabase;
+
+/**
+ * The main pet database class.
+ * @author Paul
+ */
+public class PetDatabase {
+    
+}
