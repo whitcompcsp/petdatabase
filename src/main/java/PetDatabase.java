67cab5c707ec04aa8c782a4c17daec0dd3ab6c32
Paul Whitcomb <whitcomp@csp.edu>
2019-05-17 18:15:25 -0500

    Initial commit (get the main file in there)

---
+++ src/main/java/PetDatabase.java
@@ -0,0 +1,14 @@
+/*
+ * Paul Whitcomb <whitcomp@csp.edu>
+ * CSC 422, Spring 2019
+ * Concordia University, St. Paul
+ * Week 1 Assignment
+ */
+
+/**
+ * The main pet database class.
+ * @author Paul
+ */
+public class PetDatabase {
+    
+}
