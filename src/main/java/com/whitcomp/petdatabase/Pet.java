74e184519f5b4d51f164acdec3a4a48686cd7da5
Paul Whitcomb <whitcomp@csp.edu>
2019-05-26 12:18:19 -0500

    Have a setNameAndAge function in pets

--- src/main/java/com/whitcomp/petdatabase/Pet.java
+++ src/main/java/com/whitcomp/petdatabase/Pet.java
@@ -7,6 +7,9 @@
  */
 package com.whitcomp.petdatabase;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  * Pet class for holding pet data
  * @author Paul
@@ -26,15 +29,20 @@ public class Pet {
      * Get the current age of the pet
      * @return Age of the pet in years
      */
-    public int getAge() {
+    public final int getAge() {
         return age;
     }
     
     /**
      * Set the age of the pet
-     * @param age New age of the pet in years
+     * @param age New age of the pet in years. Must be between MINIMUM_AGE and MAXIMUM_AGE
      */
-    public void setAge(int age) {
+    public final void setAge(int age) {
+        // Make sure the age is valid
+        if(age < MINIMUM_AGE || age > MAXIMUM_AGE) {
+            throw new IllegalArgumentException(String.format("%d is not a valid age.", age));
+        }
+        
         this.age = age;
     }
     
@@ -42,7 +50,7 @@ public class Pet {
      * Get the current name of the pet
      * @return name of the pet
      */
-    public String getName() {
+    public final String getName() {
         return name;
     }
     
@@ -50,7 +58,7 @@ public class Pet {
      * Set the name of the pet
      * @param name New name of the pet
      */
-    public void setName(String name) {
+    public final void setName(String name) {
         this.name = name;
     }
     
@@ -60,13 +68,55 @@ public class Pet {
      * @param age Age of the pet in years. Must be between MINIMUM_AGE and MAXIMUM_AGE
      */
     public Pet(String name, int age) {
-        // Make sure the age is valid
-        if(age < MINIMUM_AGE || age > MAXIMUM_AGE) {
-            throw new IllegalArgumentException(String.format("%d is not a valid age.", age));
+        this.setAge(age);
+        this.setName(name);
+    }
+    
+    /**
+     * Constructor for pet
+     */
+    private Pet() {}
+    
+    /**
+     * Constructor for a pet. This is internally used with input from standard input in the main function.
+     * @param nameAge Name and age of the pet separated with a space. Age must be between MINIMUM_AGE and MAXIMUM_AGE.
+     * @return pet
+     */
+    static Pet petFromNameAndAge(String nameAge) {
+        Pet pet = new Pet();
+        pet.setNameAndAge(nameAge);
+        return pet;
+    }
+    
+    /**
+     * Set pet data. This is internally used with input from standard input in the main function.
+     * @param nameAge Name and age of the pet separated with a space. Age must be between MINIMUM_AGE and MAXIMUM_AGE.
+     */
+    void setNameAndAge(String nameAge) {
+        // Interrogate it with a Scanner
+        Scanner scanner = new Scanner(nameAge);
+        
+        // Get the name and age
+        String newName;
+        int newAge;
+        try {
+            newName = scanner.next();
+            newAge = scanner.nextInt();
         }
         
-        this.age = age;
-        this.name = name;
+        // If not, throw an IllegalArgumentException
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException(String.format("%s is not valid input.", nameAge));
+        }
+        
+        // Make sure there's nothing else to add
+        if(scanner.hasNext()) {
+            throw new IllegalArgumentException(String.format("%s is not valid input.", nameAge));
+        }
+        
+        // Set the new values
+        this.setAge(newAge);
+        this.setName(newName);
     }
     
     /**
